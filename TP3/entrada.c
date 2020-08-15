18efe13b963ded178625d6f48ff52608b4dbf909
Sabrina Garcia <mvlts9819@gmail.com>
2020-08-15 04:56:20 -0300

    ctes decimales ya esta

---
+++ TP3/entrada.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int aaa=15 ,BB=0x23,ccc=45,AA=03,b=021,CCC=52,D,d;
+    //hola como estas
+AA=8,04,0X51;
+/*como va
+    */
+
+   int a;
+  printf("gola");
+  puts("chau");
+   //jojo
+}
+/*ohla
+sabias que mana
+si nos e
+como q no
+*/
