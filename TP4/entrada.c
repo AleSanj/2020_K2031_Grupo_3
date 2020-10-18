7f8fc03e18114fcf1903235ed16a84a337abf374
AleSanj <63313919+AleSanj@users.noreply.github.com>
2020-10-17 21:27:13 -0300

    terminado

--- TP4/entrada.c
+++ TP4/entrada.c
@@ -1,12 +1,39 @@
+char a = "asdasd";
+
 int a=2;;{a=sizeof(char);{;;;;};;3+2;2+2;;{int a;}{}{sizeof(char)+3;}}
 if (2+3){
  if (a==b) {
         return 2;
- {}
+ {int main(char b){
+     return 2;
+ }}
+int profesantielmejor(char*,int);
  }
  while(1){
      for(;;){
          a+b;
      }
  }
+ char b = 'p';
+ char* a = "asdasd";
+ int a,b=sizeof(int),c,d,e,f,g,h,i,j,k,l,m,n;
+}
+struct alo{
+int santi;
+char sabri;
+int ale;
+int mati;
+float juli;
+int iniaki;
+}aprobados;
+
+switch (2)
+{
+    case 1: 
+        break;
+    case 2: 
+        break;
+    default: 
+
+    return 0;
 }
