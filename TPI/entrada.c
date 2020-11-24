f2a0e58f5395f8bf0f21a54781916c31f6b3dd4b
AleSanj <63313919+AleSanj@users.noreply.github.com>
2020-11-24 14:20:29 -0300

    Add files via upload

---
+++ TPI/entrada.c
@@ -0,0 +1,28 @@
+
+int n,l,k,j,h,g,f;
+int a,b;
+char c;
+float o = 2.90;
+int suma();
+void* santi(char* el, char* mejor, char* profe);
+char sabri();
+void* inia(int);
+float juli (int,int,char d);
+void mati(char w);
+int ale(int*);
+while (m){
+
+a*z*a*b*c;
+}
+// suma();
+
+// 
+
+int main(){
+    
+
+    return 1;
+}
+
+
+// no sabemos por que no podemos invocar una funcion sin parametros, nos tira error sintactico y esta en la linea 245 de la gramatica
